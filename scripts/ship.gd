0c80dc9e8be46fdb90a10d11f57c034ddbd058ac
Matt Poremba <matthew.poremba1@gmail.com>
2024-10-22 18:19:51 -0400

    Rotation WIP

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -5,8 +5,9 @@ const FORWARD_THRUST = 200.0
 const REVERSE_THRUST = 100.0
 const STRAFE_THRUST = 150.0
 const MAX_SPEED = 1000.0
+const YAW_SPEED = 100.0
 
-var vector = Vector2(0.0, 0.0)
+var ship_vector = Vector2(0.0, 0.0)
 
 @onready var label = $Label
 
@@ -15,32 +16,39 @@ func _physics_process(delta):
 	# Godot treats "up" as negative?
 	var y_thrust = Input.get_axis("reverse_thrust", "forward_thrust")
 	if y_thrust && y_thrust > 0.0:
-		print("forward!")
-		vector.y += y_thrust * FORWARD_THRUST * delta * -1.0
+		ship_vector.y += y_thrust * FORWARD_THRUST * delta * -1.0
 	elif y_thrust && y_thrust < 0.0:
-		print("reverse!")
-		vector.y += y_thrust * REVERSE_THRUST * delta * -1.0
+		ship_vector.y += y_thrust * REVERSE_THRUST * delta * -1.0
 	
 	var x_thrust = Input.get_axis("left_strafe", "right_strafe")
 	if x_thrust:
-		vector.x += x_thrust * STRAFE_THRUST * delta
+		ship_vector.x += x_thrust * STRAFE_THRUST * delta
 	
 	# Handle space braking
 	if Input.is_action_pressed("space_brake"):
-		if vector.x > 0.0:
-			vector.x -= STRAFE_THRUST * delta
-		elif vector.x < 0.0:
-			vector.x += STRAFE_THRUST * delta
-		if vector.y < 0.0:
-			vector.y += REVERSE_THRUST * delta
-			if vector.y > 0.0:
-				vector.y = 0.0
-		elif vector.y > 0.0:
-			vector.y -= FORWARD_THRUST * delta
-			if vector.y < 0.0:
-				vector.y = 0.0
+		if ship_vector.x > 0.0:
+			ship_vector.x -= STRAFE_THRUST * delta
+		elif ship_vector.x < 0.0:
+			ship_vector.x += STRAFE_THRUST * delta
+		if ship_vector.y < 0.0:
+			ship_vector.y += REVERSE_THRUST * delta
+			if ship_vector.y > 0.0:
+				ship_vector.y = 0.0
+		elif ship_vector.y > 0.0:
+			ship_vector.y -= FORWARD_THRUST * delta
+			if ship_vector.y < 0.0:
+				ship_vector.y = 0.0
 	
-	velocity = vector
 
+	label.text = "vel: " + str(velocity) + ", vec: " + str(ship_vector)
+	
+	# Handle yaw
+	var mouseInput = Input.get_last_mouse_velocity()
+	if mouseInput:
+		if mouseInput.x > 0:
+			rotation_degrees += YAW_SPEED * delta
+		elif mouseInput.x < 0:
+			rotation_degrees -= YAW_SPEED * delta
+	
+	velocity = ship_vector.rotated(rotation_degrees)
 	move_and_slide()
-	label.text = "vel: " + str(velocity) + ", vec: " + str(vector)
