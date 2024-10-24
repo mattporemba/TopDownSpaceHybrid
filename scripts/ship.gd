da99672166a7032b147ab8f14586381224d13341
Matt Poremba <matthew.poremba1@gmail.com>
2024-10-23 20:20:36 -0400

    Controller complete

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -1,34 +1,30 @@
 extends CharacterBody2D
 
-
 const FORWARD_THRUST = 200.0
 const REVERSE_THRUST = 100.0
 const STRAFE_THRUST = 150.0
 const MAX_SPEED = 1000.0
 const YAW_SPEED = 100.0
 
-const MOUSE_ROT_OFFSET = 90.0
-
 var ship_vector = Vector2(0.0, 0.0)
 
 @onready var label = $Label
+
 
 func _physics_process(delta):
 	# TODO: Max yaw speed
 	look_at(get_global_mouse_position())
 	
 	# Handle space braking or translational movement.
-	var thrust_vector
 	if Input.is_action_pressed("space_brake"):
-		thrust_vector = handle_space_brake()
+		handle_space_brake(delta)
 	else:
-		thrust_vector = handle_translational_movement()
+		var thrust_vector = handle_translational_movement()
+		ship_vector += thrust_vector.rotated(rotation)
+		velocity = ship_vector * delta
+		move_and_slide()
 	
-	ship_vector += thrust_vector.rotated(rotation)
-	velocity = ship_vector * delta
-	move_and_slide()
-	
-	label.text = "vel: " + str(velocity)
+	label.text = "vel x: " + str(velocity.x) + "\nvel y : " + str(velocity.y) + "\nship x: " + str(ship_vector.x) + "\nship y: " + str(ship_vector.y)
 
 
 func handle_translational_movement() -> Vector2:
@@ -49,19 +45,29 @@ func handle_translational_movement() -> Vector2:
 	return thrust_vector
 
 
-func handle_space_brake() -> Vector2:
-	# Again, acount for have velocity x and y reversed.
-	var brake_vector = Vector2(0.0, 0.0)
-	if velocity.x > 0.0:
-		brake_vector.x = STRAFE_THRUST * -1.0
-	elif velocity.x < 0.0:
-		brake_vector.x = STRAFE_THRUST
-	#if ship_vector.y < 0.0:
-		#thrust_vector.y += REVERSE_THRUST
-		#if thrust_vector.y > 0.0:
-			#thrust_vector.y = 0.0
-	#elif ship_vector.y > 0.0:
-		#thrust_vector.y -= FORWARD_THRUST
-		#if thrust_vector.y < 0.0:
-			#thrust_vector.y = 0.0
-	return brake_vector
+func handle_space_brake(delta):
+	# Again, acount for having velocity x and y reversed.
+	var brake_vector: Vector2
+	
+	# Lateral braking
+	if ship_vector.rotated(rotation * -1.0).y < 0.0:
+		brake_vector.y = STRAFE_THRUST
+	elif ship_vector.rotated(rotation * -1.0).y > 0.0:
+		brake_vector.y = STRAFE_THRUST * -1.0
+	
+	# Forward/reverse braking
+	if ship_vector.rotated(rotation * -1.0).x > 0.0:
+		brake_vector.x = REVERSE_THRUST * -1.0
+	elif ship_vector.rotated(rotation * -1.0).x < 0.0:
+		brake_vector.x = FORWARD_THRUST
+	
+	ship_vector += brake_vector.rotated(rotation)
+	velocity = ship_vector * delta
+	# Hard stop
+	if (abs(velocity.x) < 1.0):
+		velocity.x = 0.0
+		ship_vector.x = 0.0
+	if (abs(velocity.y) < 1.0):
+		velocity.y = 0.0
+		ship_vector.y = 0.0
+	move_and_slide()
